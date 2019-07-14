89fdde91666dcc6e371eaad32d679265e4bdce36
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-14 17:05:45 +0530

    Update SeleniumTest.java

--- src/main/test/com/invenotry/framework/SeleniumTest.java
+++ src/main/test/com/invenotry/framework/SeleniumTest.java
@@ -37,9 +37,9 @@ public class SeleniumTest {
 	static ExtentTest logger;
 	static WebDriver driver;
 	WebDriverWait wait;
-	static String HUB_URL = "http://ec2-18-216-169-181.us-east-2.compute.amazonaws.com:8082/wd/hub";
+	static String HUB_URL = "http://ec2-3-15-143-59.us-east-2.compute.amazonaws.com:8082/wd/hub";
 	
-	static String APPLICATION_URL = "http://ec2-18-216-169-181.us-east-2.compute.amazonaws.com:8081/InventoryManagement/";
+	static String APPLICATION_URL = "http://ec2-3-15-143-59.us-east-2.compute.amazonaws.com:8080/InventoryManagement/";
 	
 	@BeforeTest
 	public void beforeTest() throws MalformedURLException  {
